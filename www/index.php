426cb8d7d0e066d489ce3138b7c9a9119167e6d5
段路建 <duanlujian@mafengwo.com>
2021-03-17 16:48:36 +0800

    5集群

--- www/index.php
+++ www/index.php
@@ -1,4 +1,8 @@
 <?php
 phpinfo();
-$con = $connection = new PDO("mysql:dbname=mysql;host=db", 'root', 111111);
-var_dump($con);
+$redis = new Redis();
+$redis->connect('redis', 6379); //连接Redis
+$redis->auth('310c8cabcdefghf2d8abcdefd44496ac80'); //密码验证
+$redis->select(1);//选择数据库1
+$redis->set( "testKey" , "Hello Redis"); //设置测试key
+echo $redis->get("testKey");//输出value
